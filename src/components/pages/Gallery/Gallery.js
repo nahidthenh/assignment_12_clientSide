812aff27043fd0f5aa783c0d2bf50a3108c52dfd
nahidthenh <nhgroup18@gmail.com>
2021-11-15 16:50:27 +0600

    Cta area

--- src/components/pages/Gallery/Gallery.js
+++ src/components/pages/Gallery/Gallery.js
@@ -4,12 +4,15 @@ import "./Gallery.css";
 const Gallery = () => {
   return (
     <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="cta-area">
-              <h3>Click here to Explore More</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, quas.</p>
+      <div className="cta-bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="cta-area">
+                <h3>Click here to Explore More</h3>
+                <p>Currently, there are 63 bike manufacturers in India with 203 bike models in the market. Major Brands selling Bikes in India are Hero, Honda, Suzuki, TVS, Royal Enfield. And Some of the popular bikes in India are KTM RC 200, Honda Shine, Royal Enfield Meteor 350, KTM RC 390, Hero Splendor Plus.</p>
+                <button className="buy-now-btn">Buy Now</button>
+              </div>
             </div>
           </div>
         </div>
